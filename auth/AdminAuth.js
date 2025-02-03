a75d66996a843948ad2bfb409d872c43c92307fc
kunal <kunalbhalerao789@gmail.com>
2025-02-03 22:45:28 +0530

    Created userAuth and adminAuth endpoint

---
+++ auth/AdminAuth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const { JWT_ADMIN_PASSWORD } = require("../config");
+
+function adminAuth(req, res, next) {
+  const token = req.headers.token;
+
+  const decodedData = jwt.verify(token, JWT_ADMIN_PASSWORD);
+
+  if (decodedData) {
+    req.userId = decodedData.id;
+    next();
+  } else {
+    res.status(403).json({
+      message: "Something Went Wrong",
+    });
+  }
+}
+
+module.exports = {
+  adminAuth,
+};
